3b60220e9bec84f3487b155ba0cf613c04595343
JennyShalai <jenny.shalai@gmail.com>
2016-07-07 14:28:36 -0400

    tests passed but light grey

--- ArrayChallengeDeuce2/ViewController.swift
+++ ArrayChallengeDeuce2/ViewController.swift
@@ -15,12 +15,47 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
-
-   
+    
+    func addNameToDeliLine(name: String) -> String {
+        if name == "Billy Crystal" {
+            deliLine.insert(name, atIndex: 0)
+            return "Welcome Billy! You can sit wherever you like."
+        } else if name == "Meg Ryan" {
+            deliLine.insert(name, atIndex: 0)
+            return "Welcome Meg! You can sit wherever you like."
+        } else {
+            deliLine.append(name)
+            if deliLine.count == 1 {
+                return "Welcome \(name), you're first in line!"
+            } else {
+                return "Welcome \(name), you're number \(deliLine.count) in line."
+            }
+        }
+    }
     
     
-    // Create your methods here
+    func nowServing() -> String {
+        if deliLine.isEmpty {
+            return "There is no-one to be served."
+        } else {
+            return "Now serving \(deliLine.last)!"
+        }
+    }
+    
+    
+    func deliLineDescription() -> String {
+        if deliLine.isEmpty {
+            return "The line is currently empty."
+        } else {
+            var result: String = ""
+            for (index, name) in deliLine.enumerate() {
+                result = "\(result)\(index). \(name)"
+            }
+            return "The line is: \(result)"
+        }
+    }
 
 
 }
